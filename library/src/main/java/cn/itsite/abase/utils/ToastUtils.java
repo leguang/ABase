ea3ce05a134366125ec9b70e9b9cb49dbb48841b
Administrator <admin@example.com>
2019-09-30 23:27:04 +0800

    删除无用Utils

--- library/src/main/java/cn/itsite/abase/utils/ToastUtils.java
+++ library/src/main/java/cn/itsite/abase/utils/ToastUtils.java
@@ -3,20 +3,28 @@ package cn.itsite.abase.utils;
 import android.content.Context;
 import android.widget.Toast;
 
+import androidx.annotation.StringRes;
+
 /**
  * Author：leguang on 2016/10/9 0009 15:49
  * Email：langmanleguang@qq.com
  */
 public class ToastUtils {
-    private static final String TAG = ToastUtils.class.getSimpleName();
-
     public static Toast mToast;
 
-    public static void showToast(Context mContext, String msg) {
+    public static void showToast(Context mContext, CharSequence msg) {
         if (mToast == null) {
             mToast = Toast.makeText(mContext, "", Toast.LENGTH_SHORT);
         }
         mToast.setText(msg);
+        mToast.show();
+    }
+
+    public static void showToast(Context mContext, @StringRes int resId) {
+        if (mToast == null) {
+            mToast = Toast.makeText(mContext, "", Toast.LENGTH_SHORT);
+        }
+        mToast.setText(resId);
         mToast.show();
     }
 }
